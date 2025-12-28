ed60f1349d3333be4bd2ce72654db956740475c3
DevopsAkhila <awslabs.03@gmail.com>
2025-12-28 22:59:08 +0530

    Update App.java

--- Lab5:JenkinsAgents-Controllers-Dynamic Agents-Docker-Pipelines/java-app/src/main/java/com/example/App.java
+++ Lab5:JenkinsAgents-Controllers-Dynamic Agents-Docker-Pipelines/java-app/src/main/java/com/example/App.java
@@ -7,6 +7,6 @@ public class App {
     }
 
     public static void main(String[] args) {
-        System.out.println("Java app running on Agent");
+        System.out.println("Java app running on Jenkins Docker Agent");
     }
 }
