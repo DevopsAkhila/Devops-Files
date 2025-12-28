40c521714f1577ff80922724a511db5865c8ba69
DevopsAkhila <awslabs.03@gmail.com>
2025-12-28 23:21:50 +0530

    Delete Lab5:JenkinsAgents-Controllers-Dynamic Agents-Docker-Pipelines/python-app directory

--- Lab5:JenkinsAgents-Controllers-Dynamic Agents-Docker-Pipelines/python-app/__init__.py
+++
@@ -1,1 +0,0 @@
-python-app/__init__.py
--- Lab5:JenkinsAgents-Controllers-Dynamic Agents-Docker-Pipelines/python-app/app.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-def fetch_status():
-    response = requests.get("https://httpbin.org/status/200")
-    return response.status_code
-
-if __name__ == "__main__":
-    print("HTTP Status:", fetch_status())
