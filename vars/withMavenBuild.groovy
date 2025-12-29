25696b4afe3cedf07fb869b8318b818aaa78500b
DevopsAkhila <awslabs.03@gmail.com>
2025-12-29 19:33:28 +0530

    Update withMavenBuild.groovy

--- vars/withMavenBuild.groovy
+++ vars/withMavenBuild.groovy
@@ -1,12 +1,18 @@
 def call(Closure body) {
+
+    // Use Jenkins-managed Maven
+    def mvnHome = tool 'maven-3.9'
+
     try {
         echo "Starting Maven build"
-        sh 'mvn clean package'
+        sh "${mvnHome}/bin/mvn clean package"
         body()
     } catch (err) {
         notifySlack("Build failed", "ERROR")
         throw err
     } finally {
-        archiveArtifacts artifacts: 'target/*.jar', fingerprint: true
+        archiveArtifacts artifacts: 'target/*.jar',
+                        fingerprint: true,
+                        allowEmptyArchive: true
     }
 }
